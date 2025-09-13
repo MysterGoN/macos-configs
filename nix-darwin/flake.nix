10c60487f066b68bb63a87c9d327afb5724e4173
Nikolay Granenkov <nickgran@yandex.ru>
2025-09-13 17:54:52 +0300

    feat: Add nix-darwing fingerprint

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -26,6 +26,9 @@
       # Set Git commit hash for darwin-version.
       system.configurationRevision = self.rev or self.dirtyRev or null;
 
+      # Fingerprint
+      security.pam.services.sudo_local.touchIdAuth = true;
+
       # Used for backwards compatibility, please read the changelog before changing.
       # $ darwin-rebuild changelog
       system.stateVersion = 6;
